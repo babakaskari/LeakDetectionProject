2171d53c9754487f3c36983bdd442aaf5cabeaa1
mohammj <babakas76@gmail.com>
2020-12-04 03:17:36 +0100

    CHANGED TO NORMALIZED

--- algorithm/k_means.py
+++ algorithm/k_means.py
@@ -1,28 +1,18 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-from matplotlib import dates as mpl_dates
 import sklearn
 from sklearn.metrics import roc_auc_score
 from sklearn.semi_supervised import LabelPropagation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_classification
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_validate
-from sklearn.metrics import roc_curve
-from sklearn.semi_supervised import LabelPropagation
 from sklearn.cluster import KMeans
 import seaborn as sns
 from sklearn import metrics
