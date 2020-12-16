f7feb7c981477d34acac4450d1381dc0d1192a99
mohammj <babakas76@gmail.com>
2020-12-16 13:15:29 +0100

    change 16 Dec again

--- algorithm/Semi_Supervised/semi_random_forest.py
+++ algorithm/Semi_Supervised/semi_random_forest.py
@@ -7,6 +7,7 @@ from sklearn import preprocessing, metrics
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 import sklearn
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
@@ -16,14 +17,13 @@ from xgboost import XGBClassifier
 import prepossessed_dataset
 
 pd.set_option('mode.chained_assignment', None)
-
 dataset = prepossessed_dataset.semi_super()
-X_unlabeled = dataset["X_unlabeled"]
-
+x_unlabeled = dataset["x_unlabeled"]
 x_train = dataset["x_train"]
 y_train = dataset["y_train"]
 x_test = dataset["x_test"]
 y_test = dataset["y_test"]
+
 
 def evaluate_preds(model, x_true, y_true, y_preds):
     accuracy = metrics.accuracy_score(y_true, y_preds)
@@ -41,8 +41,9 @@ def evaluate_preds(model, x_true, y_true, y_preds):
     print(f"F1 Score : {f1: .2f}")
 
     return metric_dict
-
 # Initiate iteration counter
+
+
 iterations = 0
 
 # Containers to hold f1_scores and # of pseudo-labels
@@ -54,14 +55,14 @@ pseudo_labels = []
 high_prob = [1]
 
 # Loop will run until there are no more high-probability pseudo-labels
-while len(high_prob) > 0:
+while len(high_prob) > 0 and len(x_unlabeled) > 0:
     # Fit classifier and make train/test predictions
     # print(y_train)
     # clf = LogisticRegression(max_iter=10000)
     # clf.fit(X_train, y_train.values.ravel())
 
     # #######################################  XGBClassifier()
-    clf = XGBClassifier()
+    clf = clf_rfc = RandomForestClassifier()
     clf.fit(x_train, y_train)
     # xgb_pred = clf.predict(x_train)
     # xgb_matrices = evaluate_preds(clf, x_train, y_test, xgb_pred)
@@ -72,10 +73,6 @@ while len(high_prob) > 0:
     train_f1 = f1_score(y_train, y_hat_train, average='micro')
     test_f1 = f1_score(y_test, y_hat_test, average='micro')
     print(f"Iteration {iterations}")
-
-
-
-
     print(f"Train f1: {train_f1}")
     print(f"Test f1: {test_f1}")
     train_f1s.append(train_f1)
@@ -84,17 +81,16 @@ while len(high_prob) > 0:
     # Generate predictions and probabilities for unlabeled data
     print(f"Now predicting labels for unlabeled data...")
 
-    pred_probs = clf.predict_proba(X_unlabeled)
-    preds = clf.predict(X_unlabeled)
+    pred_probs = clf.predict_proba(x_unlabeled)
+    preds = clf.predict(x_unlabeled)
     prob_0 = pred_probs[:, 0]
     prob_1 = pred_probs[:, 1]
-
     # Store predictions and probabilities in dataframe
     df_pred_prob = pd.DataFrame([])
     df_pred_prob['Leak Found'] = preds
     df_pred_prob['prob_0'] = prob_0
     df_pred_prob['prob_1'] = prob_1
-    df_pred_prob.index = X_unlabeled.index
+    df_pred_prob.index = x_unlabeled.index
     # Separate predictions with > 99% probability
     high_prob = pd.concat([df_pred_prob.loc[df_pred_prob['prob_0'] > 0.99],
                            df_pred_prob.loc[df_pred_prob['prob_1'] > 0.99]],
@@ -105,14 +101,15 @@ while len(high_prob) > 0:
     pseudo_labels.append(len(high_prob))
 
     # Add pseudo-labeled data to training data
-    X_train = pd.concat([x_train, X_unlabeled.loc[high_prob.index]], axis=0)
+    x_train = pd.concat([x_train, x_unlabeled.loc[high_prob.index]], axis=0)
     high_prob = high_prob.drop(columns=['prob_0', 'prob_1'])
     print(high_prob)
 
     y_train = pd.concat([y_train, high_prob])
+
     # Drop pseudo-labeled instances from unlabeled data
-    X_unlabeled = X_unlabeled.drop(index=high_prob.index)
-    print(f"{len(X_unlabeled)} unlabeled instances remaining.\n")
+    x_unlabeled = x_unlabeled.drop(index=high_prob.index)
+    print(f"{len(x_unlabeled)} unlabeled instances remaining.\n")
 
     # Update iteration counter
     iterations += 1
